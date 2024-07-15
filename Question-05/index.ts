a9f7898a7f5f6418e05e950e45dee6bd5bdc93ef
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-15 19:59:20 +0500

    Question-05 commit

---
+++ Question-05/index.ts
@@ -0,0 +1,7 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famousPerson : string = "Quaid-e-Azam Muhammad Ali Jinnah"
+
+let famousQuote : string = "Expect the best, Prepare for the worst!"
+
+console.log(`${famousPerson} once said: ${famousQuote}`);
