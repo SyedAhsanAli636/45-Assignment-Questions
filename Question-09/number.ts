5082cddb36dc40a9153a707410647e5e2baf732c
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-15 21:51:57 +0500

    Question-09 commit

---
+++ Question-09/number.ts
@@ -0,0 +1,10 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favouriteNumber : number = 10
+
+console.log(`My favorite number is: ${favouriteNumber}`);
+
+console.log(`This is my favorite number: ${favouriteNumber}`);
+
+
+
