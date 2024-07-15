465c0e6066dd4d443dbb08f8f71f49e501f2fb82
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-15 23:32:24 +0500

    Question-11 commit

---
+++ Question-12/names.js
@@ -0,0 +1,7 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let friends_name = ["Rehan", "Shahnawaz", "Asim", "Dawood"];
+let message = "Nice to meet you Buddy!";
+for (let index = 0; index < friends_name.length; index++) {
+    console.log(`${friends_name[index]}, ${message}`);
+}
+export {};
