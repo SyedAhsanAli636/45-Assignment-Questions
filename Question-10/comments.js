61b117fe109c8f4a1b28ed0b7d6ea989a41b073a
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-15 23:08:22 +0500

    Question-10 commit

---
+++ Question-10/comments.js
@@ -0,0 +1,7 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+console.log("Date: 01-07-2024, \nSyed Ahsan Ali");
+let program_1 = "Calculator";
+console.log(`${program_1} : Something used for making Mathematical Calculations.`);
+let program_2 = "Television";
+console.log(`${program_2} : A system for converting visual images (with sound) into electrical signals.`);
+export {};
