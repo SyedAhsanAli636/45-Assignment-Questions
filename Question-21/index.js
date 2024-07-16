23940907c6c02488cb595fe28d90dc8f1bdd452d
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-16 18:29:43 +0500

    Question-21 commit

---
+++ Question-21/index.js
@@ -0,0 +1,10 @@
+// Question No. 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let details = {
+    name: "Syed Ahsan Ali",
+    age: 32,
+    address: "Karachi",
+    isStudent: true
+};
+console.log(details);
+console.log(details.isStudent);
+export {};
---
+++ Question-21/index.ts
@@ -0,0 +1,13 @@
+// Question No. 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let details : {name:string; age:number; address:string; isStudent:boolean} = {
+
+    name : "Syed Ahsan Ali",
+    age : 32,
+    address : "Karachi",
+    isStudent : true
+}
+console.log(details);
+console.log(details.isStudent);
+
+
