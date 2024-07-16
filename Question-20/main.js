40fa286a2702635d1a92b5080fc690bd1dd66dfb
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-16 18:20:41 +0500

    Question-20 commit

---
+++ Question-20/main.js
@@ -0,0 +1,4 @@
+// Question No. 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let places = ["K2", "Amazon River", "Canada", "Barcelona", "German", "Sydney Harbour Bridge"];
+console.log(`These are my favourite famous places in the world: ${places}.`);
+export {};
