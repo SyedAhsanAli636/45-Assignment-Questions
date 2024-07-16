042b9b31fbc4562f5e098a46611a4b69ba2fbcee
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-16 20:06:51 +0500

    Question-22 commit

---
+++ Question-22/error.js
@@ -0,0 +1,5 @@
+// Question No. 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let error = ["Ahsan", 32, false];
+console.log(error[3]);
+console.log(error[1]);
+export {};
---
+++ Question-22/error.ts
@@ -0,0 +1,5 @@
+// Question No. 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+let error : (string|number|boolean) [] = ["Ahsan", 32, false]
+console.log(error[3]);
+console.log(error[1]);
