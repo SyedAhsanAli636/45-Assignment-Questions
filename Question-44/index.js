54e5dbbbaa2f039987c84866af9501db23d12403
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-19 19:40:31 +0500

    Question-44 commit

---
+++ Question-44/index.js
@@ -0,0 +1,10 @@
+// Question No. 44. Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+function sandwich(array) {
+    console.log(`Your order is ready included with:`);
+    console.log(`${array}`);
+    console.log(`Enjoy your Meal !`);
+}
+sandwich(["Extra Cheez", "Salad", "Chicken Piece"]);
+sandwich(["Extra Toopings", "Drink", "French Fries"]);
+sandwich(["Extra Sause", "Ketchup", "Crispy Chicken"]);
+export {};
