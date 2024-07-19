7e25e5b713aed9b0dd65ff888a7553d48ae2e584
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-19 16:08:30 +0500

    Question-40 commit

---
+++ Question-40/album.ts
@@ -0,0 +1,17 @@
+//Question No. 40. Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
+
+function make_album(artist : string, title : string, tracks? : number ) {
+ 
+    let album : {artist : string, title : string, tracks? : number} = {   
+        
+        artist : artist,
+        title : title,
+        tracks : tracks
+    }
+    return album
+}
+console.log(make_album("Dharia", "August Diaries", 5));
+
+console.log(make_album("Strings", "Dhaani", 6));
+
+console.log(make_album("Alan Walker", "Faded"));
