e529dbf796389b838cdf9a2e2d5046ae65784504
Syed Ahsan Ali <167331258+SyedAhsanAli636@users.noreply.github.com>
2024-07-21 14:52:19 +0500

    Question-45 commit

---
+++ Question-45/index.ts
@@ -0,0 +1,14 @@
+// Question No. 45. Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+
+function car(manufacturer:string, model:string, ...rest:{[key : string]:any}[]) {
+
+    let car= {
+        manufacturer : manufacturer,
+        model : model,
+        ...Object.assign({},...rest)
+};
+    return car;
+}
+let my_car = car("Toyota", "Corrolla", {colour:"Black"})
+
+console.log(my_car);
